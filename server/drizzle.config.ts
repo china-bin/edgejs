16de8c852fd02a591e54cd0fba1cb1dab41f37de
liuzongbin <958615915@qq.com>
2024-05-23 16:20:58 +0800

    init

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,6 +1,8 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-} satisfies Config;
+  dialect: "sqlite"
+});
+
